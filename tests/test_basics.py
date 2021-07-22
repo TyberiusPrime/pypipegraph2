d47c4bfbbfac187c36e5bcbba5bb8c365f702e6d
Unknown <finkernagel@imt.uni-marburg.de>
2021-07-22 15:49:32 +0200

    adjust test case for latest symlink

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1452,23 +1452,23 @@ class TestPypipegraph2:
             assert len(list(ppg.global_pipegraph.log_dir.glob("*.log"))) == 0
             ppg.run()
             assert (
-                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 1 + 1
+                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 1 + 1 + 1 # for latest
             )  # runtimes
             ppg.run()
             assert (
-                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 2 + 1
+                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 2 + 1 + 1 # for latest
             )  # runtimes
             ppg.new(log_retention=2)
             ppg.run()
             prior = list(ppg.global_pipegraph.log_dir.glob("*"))
             assert (
-                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 3 + 1
+                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 3 + 1 + 1 # for latest
             )  # runtimes
             # no new.. still new log file please
             ppg.run()
             after = list(ppg.global_pipegraph.log_dir.glob("*"))
             assert (
-                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 3 + 1
+                len(list(ppg.global_pipegraph.log_dir.glob("*"))) == 3 + 1 + 1 # for latest
             )  # runtimes
             assert set([x.name for x in prior]) != set([x.name for x in after])
 
