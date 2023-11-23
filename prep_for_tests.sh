22f3fb1d89cb6ff1badd126b9acc422d5ad13661
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2023-11-23 13:20:25 +0100

    support interactive testing on python 3.10

--- prep_for_tests.sh
+++ prep_for_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 CARGO_TARGET_DIR=target cargo build
 cp target/debug/libpypipegraph2.so python/pypipegraph2/pypipegraph2.cpython-39-x86_64-linux-gnu.so
+cp target/debug/libpypipegraph2.so python/pypipegraph2/pypipegraph2.cpython-310-x86_64-linux-gnu.so
 echo "Ready"
 
 
