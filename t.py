ce1be42304f3698ce40eca137ed9692162b89822
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-05-06 11:42:35 +0200

    Prune non-source files

--- t.py
+++
@@ -1,8 +0,0 @@
-import time
-import os
-import psutil
-while True:
-    for p in psutil.process_iter():
-        if 'sleep' in p.cmdline():
-            print('pid', p.pid, p.cmdline(), 'sid', os.getpgid(p.pid))
-    time.sleep(.1)
