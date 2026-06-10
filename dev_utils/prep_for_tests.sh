72da82ec6e68a8aa6fba4bc2a796d658e93ca2e0
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2026-06-10 11:47:01 +0200

    upgrade flake to nixos 26.05, python 314, add cargo-afl make it be universal

--- dev_utils/prep_for_tests.sh
+++ dev_utils/prep_for_tests.sh
@@ -2,10 +2,7 @@
 # Prepare for python tests by copying the so where we need it
 set -eou pipefail
 CARGO_TARGET_DIR=target cargo build
-cp target/debug/libpypipegraph2.so python/pypipegraph2/pypipegraph2.cpython-39-x86_64-linux-gnu.so
-cp target/debug/libpypipegraph2.so python/pypipegraph2/pypipegraph2.cpython-310-x86_64-linux-gnu.so
-cp target/debug/libpypipegraph2.so python/pypipegraph2/pypipegraph2.cpython-311-x86_64-linux-gnu.so
-cp target/debug/libpypipegraph2.so python/pypipegraph2/pypipegraph2.cpython-312-x86_64-linux-gnu.so
+cp target/debug/libpypipegraph2.so python/pypipegraph2/pypipegraph2.abi3.so
 # echo "Ready"
 
 
